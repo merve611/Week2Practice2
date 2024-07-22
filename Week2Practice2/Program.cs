31e6093a02915e5343da857a0b31ca80079b4ec2
merve cakiroglu <mrveeckrrr@hotmail.com>
2024-07-22 10:38:33 +0300

    Add project files.

---
+++ Week2Practice2/Program.cs
@@ -0,0 +1,76 @@
+﻿
+//Konsol ekranına 10 kere "Kendime inanıyorum, ben bu yazılım işini hallederim!" yazdırınız.
+
+int sayac = 0;
+while (sayac <10)
+{
+    Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim");
+    sayac++;
+}
+
+Console.WriteLine("---------------------------");
+
+//1 ile 20 arasındaki sayıları konsol ekranına yazdırınız.
+
+int sayac2 = 0;
+while (sayac2 < 20)
+{
+    Console.WriteLine(sayac2);
+    sayac2++;
+}
+
+Console.WriteLine("----------------------------");
+
+//1 ile 20 arasındaki çift sayıları konsol ekranına yazdırınız.
+
+int sayac3 = 0;
+while (sayac3<20)
+{
+    if(sayac3 % 2 == 0)
+    {
+        Console.WriteLine(sayac3);
+    }
+    sayac3++;
+}
+
+Console.WriteLine("---------------------------");
+
+
+// 50 ile 150 arasındaki sayıların toplamını ekrana yazdırınız.
+
+int sayac4 = 50;
+int toplam = 0;
+
+while (sayac4<150)
+{
+    toplam = toplam + sayac4;
+    
+
+    sayac4++;
+}
+Console.WriteLine(toplam);
+
+Console.WriteLine("---------------------------");
+
+// 1 ile 120 arasındaki tek ve çift sayıların toplamlarını ayrı ayrı ekrana yazdırınız.
+
+int sayac5 = 1;
+int toplamT=0;
+int toplamC=0;
+
+while (sayac5<120)
+{
+    if(sayac5 % 2 == 0)
+    {
+        toplamC = toplamC + sayac5;
+
+    }
+    else
+    {
+        toplamT = toplamT + sayac5;
+    }
+    sayac5++;
+}
+Console.WriteLine("Çift sayilar toplamı : " + toplamC);
+
+Console.WriteLine("Tek sayilar toplamı : " + toplamT);
